43e14236ac61d0bbdf445749209a0a342fa98400
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-18 20:58:53 +0200

    Design Post model (1)

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content  = models.TextField(max_length=2000)
+    draft = models.BooleanField()
+    
+    def __str__(self):
+        return f'{self.title} {self.id}'
