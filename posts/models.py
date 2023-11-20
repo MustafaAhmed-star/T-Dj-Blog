0dcd0db906d6997d275c4a2f3ed9a082ca183a86
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-20 23:18:43 +0200

    add publish_date filed

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content  = models.TextField(max_length=2000)
     draft = models.BooleanField()
-    
+    publish_date = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f'{self.title} {self.id}'
