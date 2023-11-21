de0a76e1c890ab86527ecab012e06a8052c64693
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-21 19:45:30 +0200

    adding Category to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Post  
+from .models import Post  ,Category
 
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title','category']
     list_filter = ['draft']
     search_fields  = ['title']
 admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
