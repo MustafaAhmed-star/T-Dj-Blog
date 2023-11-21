46aac9fb93f0e08f29eeec25b460905db42b1213
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-21 22:37:58 +0200

    add comment model

--- posts/admin.py
+++ posts/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Post  ,Category
+from .models import Post,Category,Comment
 
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title','category']
+    list_display = ['title']
     list_filter = ['draft']
     search_fields  = ['title']
 admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
+admin.site.register(Comment)
--- posts/models.py
+++ posts/models.py
@@ -21,3 +21,11 @@ class Category(models.Model):
     
     def __str__(self) -> str:
         return self.name
+        
+class Comment(models.Model):
+    post = models.ForeignKey(Post,related_name='comment_post',on_delete=models.CASCADE)
+    user = models.CharField(max_length=50)
+    comment = models.TextField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.user
