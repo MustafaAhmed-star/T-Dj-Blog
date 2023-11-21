8af805be71e5347e719314597e4fb224c1fad825
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-21 13:40:20 +0200

    add author to post (Forignkey)

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from taggit.managers import TaggableManager
-
+from django.contrib.auth.models import User
 
 class Post(models.Model):
+    author = models.ForeignKey(User,related_name='post_author' ,on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    content  = models.TextField(max_length=2000)
+    content  = models.TextField(max_length=2000)# I can add TextField without max_length
     draft = models.BooleanField()
     publish_date = models.DateTimeField(auto_now=True)
     tags = TaggableManager()
     image = models.ImageField(default ='1.png')
+    
+    
     def __str__(self):
         return f'{self.title} {self.id}'
