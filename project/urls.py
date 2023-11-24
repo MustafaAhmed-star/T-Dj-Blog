35d1454a7899099bbea86e5d0d99f0ce94b072b7
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-24 21:23:35 +0200

    add the url and apply context in template

--- project/urls.py
+++ project/urls.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from posts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/',views.post_list),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
