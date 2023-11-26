4955cd9d6290950f00d9c8027894b86202f23037
Mustafa  Abdelmgeed <67156516+MustafaAhmed-star@users.noreply.github.com>
2023-11-26 13:27:34 +0200

    add editor (summernote) to content field

--- posts/admin.py
+++ posts/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import Post,Category,Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 
-
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ['title']
     list_filter = ['draft']
     search_fields  = ['title']
-admin.site.register(Post,PostAdmin)
+    summernote_fields = ['content']
+admin.site.register(Post ,PostAdmin)
 admin.site.register(Category)
 admin.site.register(Comment)
